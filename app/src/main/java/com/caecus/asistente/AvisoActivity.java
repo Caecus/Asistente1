dada3e278786d1c0a8b0b55367c9dce70b3ea5e2
Caecus <proyectocaecus@gmail.com>
2016-09-04 19:13:08 -0300

    Segundo Commit

--- app/src/main/java/com/caecus/asistente/AvisoActivity.java
+++ app/src/main/java/com/caecus/asistente/AvisoActivity.java
@@ -10,7 +10,6 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
